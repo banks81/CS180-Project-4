0d1dfec21072acdf9f9358328dc3fca3bfea0879
Minhy0ung <129882895+Minhy0ung@users.noreply.github.com>
2023-04-04 12:36:12 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public final String WELCOME = "Welcome to the marketplace!";
+    public final String LOGIN = "Are you an existing user?";
+    public final String YESNO = "1. Yes\n2. No";
+    public final String ENTERLOGIN = "Please enter your email address.";
+    public final String NOUSER = "No user found with given email!";
+    public final String ENTERPSWD = "Please enter your password.";
+    public final String NOPSWD = "Incorrect password!";
+    public final String HI = "Welcome back!";
+    public final String OPTIONS = "Please choose your option:";
+
+    //seller
+    public final String SELLEROPTIONS = "1. Manage Stores\n2. View sales";
+    public final String STOREOPTION = "Which store would you like to access?";
+    public final String STORESPEC = "1. Create products\n2. Modify existing products";
+    public final String NEWOPTIONS = "Would you like to import csv file?";
+    public final String FILEIMPORT = "Please enter the name of file:";
+    public final String
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+
+
+}
