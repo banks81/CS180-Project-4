ffb5a8c5857c8d2946418b127eea6251ee5b8caa
Minhy0ung <129882895+Minhy0ung@users.noreply.github.com>
2023-04-05 22:12:25 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Customer extends User {
-    private ArrayList<Products> pastPurchase;
+    private ArrayList<String> pastPurchase;
     private ArrayList<Integer> purchaseCount;
     public Customer(String email, String name, String password) {
         super(email, name, password);
@@ -9,11 +9,11 @@ public class Customer extends User {
         purchaseCount = new ArrayList<>();
     }
 
-    public ArrayList<Products> getPastPurchase() {
+    public ArrayList<String> getPastPurchase() {
         return pastPurchase;
     }
 
-    public void setPastPurchase(ArrayList<Products> pastPurchase) {
+    public void setPastPurchase(ArrayList<String> pastPurchase) {
         this.pastPurchase = pastPurchase;
     }
 
@@ -25,10 +25,10 @@ public class Customer extends User {
         this.purchaseCount = purchaseCount;
     }
 
-    public void addProducts(Products products, int purchases) {
+    public void addProducts(String products, int purchases) {
         int index = 0;
-        for (Products product : pastPurchase) {
-            if (equals(product, products)) {
+        for (String product : pastPurchase) {
+            if (product.equals(products)) {
                 purchaseCount.set(index, purchaseCount.get(index) + purchases);
                 break;
             }
@@ -38,16 +38,5 @@ public class Customer extends User {
             pastPurchase.add(products);
             purchaseCount.add(purchases);
         }
-    }
-
-    public boolean equals(Products product1, Products product2) {
-        if (product1.getName().equals(product2.getName())) {
-            if (product1.getPrice() == product2.getPrice()) {
-                if (product1.getDescription().equals(product2.getDescription())) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 }
