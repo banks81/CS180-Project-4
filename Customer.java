52eceb2a58962eb1fbd322ea3a2f7a1833052301
hughes-sofia <129882903+hughes-sofia@users.noreply.github.com>
2023-04-06 12:15:02 -0400

    Update Customer.java
    added shoppingCart array for customer

--- Customer.java
+++ Customer.java
@@ -3,10 +3,27 @@ import java.util.ArrayList;
 public class Customer extends User {
     private ArrayList<String> pastPurchase; //edit made, if the code is made under the assumption this is a Products class please let me know
     private ArrayList<Integer> purchaseCount;
+    private ArrayList<Products> shoppingCart;
+
     public Customer(String email, String name, String password) {
         super(email, name, password);
         pastPurchase = new ArrayList<>();
         purchaseCount = new ArrayList<>();
+        shoppingCart = new ArrayList<>();
+
+    }
+    public ArrayList<Products> getShoppingCart() {
+        return shoppingCart;
+    }
+    public void addToShoppingCart(Products products) {
+        shoppingCart.add(products);
+    }
+    public void removeFromShoppingCart(Products products) {
+        for (int i = 0; i < shoppingCart.size(); i++) {
+            if (products.equals(shoppingCart.get(i))) {
+                shoppingCart.remove(shoppingCart.get(i));
+            }
+        }
     }
 
     public ArrayList<String> getPastPurchase() {
