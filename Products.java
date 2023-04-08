b4d4f0a7b5e728fd8a5e467ba9ecd9792f58037a
banks81 <129882803+banks81@users.noreply.github.com>
2023-04-08 17:30:38 -0400

    Update Products.java

--- Products.java
+++ Products.java
@@ -67,6 +67,74 @@ public class Products {
                            "\n              Product Description: " + getDescription()
                 + "\n              Quantity Available: " + getQuantity() + ", Price: " + getPrice());
     }
+    public int checkChoice(Scanner scan, int lastNum) {
+        int choice;
+        do {
+            try {
+                choice = Integer.parseInt(scan.nextLine());
+                if (choice > 0 && choice < lastNum + 1) {
+                    return choice;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid option!");
+            }
+        } while (true);
     }
+
+    public int checkInt(Scanner scan) {
+        int choice;
+        do {
+            try {
+                choice = Integer.parseInt(scan.nextLine());
+                return choice;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid option!");
+            }
+        } while (true);
+    }
+
+    public double checkDouble(Scanner scan) {
+        double choice;
+        do {
+            try {
+                choice = Double.parseDouble(scan.nextLine());
+                return choice;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid option!");
+            }
+        } while (true);
+    }
+    public void editProduct(Scanner scan) {
+        int choice;
+        System.out.println("Edit name?\n1. Yes\n2. No");
+        choice = checkChoice(scan, 2);
+        if (choice == 1) {
+            System.out.println("Enter the name of the product: ");
+            String productName = scan.nextLine();
+            setName(productName);
+        }
+        System.out.println("Edit description?\n1. Yes\n2. No");
+        choice = checkChoice(scan, 2);
+        if (choice == 1) {
+            System.out.println("Enter the description of the product: ");
+            String productDescription = scan.nextLine();
+            setDescription(productDescription);
+        }
+        System.out.println("Edit price?\n1. Yes\n2. No");
+        choice = checkChoice(scan, 2);
+        if (choice == 1) {
+            System.out.println("Enter the price of the product: ");
+            double price = checkDouble(scan);
+            setPrice(price);
+        }
+        System.out.println("Edit quantity?\n1. Yes\n2. No");
+        choice = checkChoice(scan, 2);
+        if (choice == 1) {
+            System.out.println("Enter the quantity of the product: ");
+            int quantity = checkInt(scan);
+            setQuantity(quantity);
+        }
+    }
+
     
 }
