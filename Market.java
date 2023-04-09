01d301ac129b9eda7d34e7496ee5c9335b01d434
Minhy0ung <129882895+Minhy0ung@users.noreply.github.com>
2023-04-09 17:53:59 -0400

    Update Market.java

--- Market.java
+++ Market.java
@@ -1073,33 +1073,35 @@ public class Market {
         try {
             FileWriter fileWriter = new FileWriter(storeFile, false);
             PrintWriter printWriter = new PrintWriter(fileWriter);
-
-            for (Store store : storesList) {
-                printWriter.println(store.getName());
-                printWriter.println(store.getSellerEmail() + "," + store.getSellerName());
-                printWriter.println(store.getRevenue());
-                printWriter.println(store.getSales());
-                if (!store.getCustomers().isEmpty()) {
-                    printWriter.println("CUSTOMERLIST");
-                    for (Customer customer : store.getCustomers()) {
-                        printWriter.println(customer.getName() + "," + customer.getEmail());
+            for (User seller : sellersList) {
+                Seller sellerClassChanged = (Seller) seller;
+                for (Store store : sellerClassChanged.getStore()) {
+                    printWriter.println(store.getName());
+                    printWriter.println(store.getSellerEmail() + "," + store.getSellerName());
+                    printWriter.println(store.getRevenue());
+                    printWriter.println(store.getSales());
+                    if (!store.getCustomers().isEmpty()) {
+                        printWriter.println("CUSTOMERLIST");
+                        for (Customer customer : store.getCustomers()) {
+                            printWriter.println(customer.getName() + "," + customer.getEmail());
+                        }
+                        printWriter.println("--------");
                     }
-                    printWriter.println("--------");
-                }
-                if (!store.getGoods().isEmpty()) {
-                    printWriter.println("PRODUCTSLIST");
-                    for (Products product : store.getGoods()) {
-                        printWriter.printf("%s,%.2f,%d,%s,%d\n",product.getName(),product.getPrice(),product.getQuantity(),product.getDescription(),product.getSales());
+                    if (!store.getGoods().isEmpty()) {
+                        printWriter.println("PRODUCTSLIST");
+                        for (Products product : store.getGoods()) {
+                            printWriter.printf("%s,%.2f,%d,%s,%d\n", product.getName(), product.getPrice(), product.getQuantity(), product.getDescription(), product.getSales());
+                        }
+                        printWriter.println("--------");
                     }
-                    printWriter.println("--------");
-                }
-                if (store.getCustomers().isEmpty() && store.getGoods().isEmpty()) {
-                    printWriter.println("--------");
+                    if (store.getCustomers().isEmpty() && store.getGoods().isEmpty()) {
+                        printWriter.println("--------");
+                    }
                 }
             }
             printWriter.close();
         } catch (Exception e) {
             System.out.println("Program terminated.");
         }
-    }    
+    }   
 }
