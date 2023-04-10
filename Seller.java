799661974f220438bce7c1ef764b577782465f88
banks81 <129882803+banks81@users.noreply.github.com>
2023-04-10 16:12:30 -0400

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -134,32 +134,4 @@ public class Seller extends User{
             }
         } while (true);
     }
-    public void assignProduct(String fileName, Store store) throws FileNotFoundException {
-        File file = new File(fileName);
-        if (!file.exists()) {
-            throw new FileNotFoundException();
-        }
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            while (true) {
-                String eachLine = bufferedReader.readLine();
-                if (eachLine == null) {
-                    break;
-                }
-                try {
-                    String[] lineBreak = eachLine.split(",");
-                    //Assuming the csv file is in name, price, quantity, description format
-                    store.addGoods(new Products(lineBreak[0], Double.parseDouble(lineBreak[1]),
-                            Integer.parseInt(lineBreak[2]), lineBreak[3], 0, store.getName(), 0)); //edit; store name added to the parameter & field of Products
-                } catch (Exception e) {                                                         //another edit made to initialize sale as int
-                    System.out.println("Wrong file format!");                                   //better when we work with initialization from file IO
-                    break;
-                }
-            }
-            bufferedReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
