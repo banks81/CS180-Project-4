dfd4d70b45b254180d9f71257647f5bcf9158834
Minhy0ung <129882895+Minhy0ung@users.noreply.github.com>
2023-04-10 14:17:40 -0400

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -151,7 +151,7 @@ public class Seller extends User{
                     String[] lineBreak = eachLine.split(",");
                     //Assuming the csv file is in name, price, quantity, description format
                     store.addGoods(new Products(lineBreak[0], Double.parseDouble(lineBreak[1]),
-                            Integer.parseInt(lineBreak[2]), lineBreak[3], 0, store.getName())); //edit; store name added to the parameter & field of Products
+                            Integer.parseInt(lineBreak[2]), lineBreak[3], 0, store.getName(), 0)); //edit; store name added to the parameter & field of Products
                 } catch (Exception e) {                                                         //another edit made to initialize sale as int
                     System.out.println("Wrong file format!");                                   //better when we work with initialization from file IO
                     break;
